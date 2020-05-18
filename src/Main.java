1e5cf05aaad09949c37fe02a68872098dd9c5de5
Porodin <Creshe2001@mail.ru>
2020-05-18 11:51:58 +0300

    Converted task

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
         long number = 600851475143L;
+        long i = 2;
 
-        for(int i = 2; i <= Math.sqrt(number); i++)
-        {
-            if(number % i == 0)
-            {
+        for (; i <= number; i++) {
+            if (number % i == 0) {
                 number = number / i;
-                i--;
             }
         }
-            System.out.println("Самый большой делитель числа 600851475143 является число " + number);
+        System.out.println("Самый большой делитель числа 600851475143 является число " + (i-1));
     }
 }
 
